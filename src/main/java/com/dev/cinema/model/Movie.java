1ede210606d44f36933b71d7bd9bab477afcfcb7
Laszlo Petechuk <frostpv@gmail.com>
2020-09-30 14:49:40 +0300

    add annotations in to Movie.java

--- src/main/java/com/dev/cinema/model/Movie.java
+++ src/main/java/com/dev/cinema/model/Movie.java
@@ -1,6 +1,16 @@
 package com.dev.cinema.model;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
