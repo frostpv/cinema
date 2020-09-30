904fb60c200848dc90c4f816fb8c1c1b237573ee
Laszlo Petechuk <frostpv@gmail.com>
2020-09-30 14:32:58 +0300

    add impl to dao and services

---
+++ src/main/java/com/dev/cinema/dao/MovieDaoImpl.java
@@ -0,0 +1,18 @@
+package com.dev.cinema.dao;
+
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.Movie;
+import java.util.List;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        return null;
+    }
+}
