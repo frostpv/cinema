5cd0d54ebed94c4f84c9ec7f4bf99df2038bbb13
Laszlo Petechuk <frostpv@gmail.com>
2020-10-06 21:24:29 +0300

    added impl MovieSessionDaoImpl

--- src/main/java/com/dev/cinema/dao/MovieSessionDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/MovieSessionDaoImpl.java
@@ -1,17 +1,55 @@
 package com.dev.cinema.dao;
 
+import com.dev.cinema.exceptions.DataProcessingException;
 import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.util.HibernateUtil;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MovieSessionDaoImpl implements MovieSessionDao {
     @Override
     public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
-        return null;
+        LocalDateTime beginFindTime = date.atStartOfDay();
+        LocalDateTime endFindTime = date.atTime(LocalTime.MAX);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> movieSessionQuery =
+                    session.createQuery("from MovieSession where movie.id = :movieId "
+                    + "and :showTime between :beginFindTime and :endFindTime", MovieSession.class);
+            movieSessionQuery.setParameter("movieId", movieId);
+            movieSessionQuery.setParameter("beginFindTime", beginFindTime);
+            movieSessionQuery.setParameter("endFindTime", endFindTime);
+            return movieSessionQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get movie sessions by movie id "
+                    + movieId + " at date: " + date.toString() + "from DB", e);
+        }
     }
 
     @Override
-    public MovieSession add(MovieSession session) {
-        return null;
+    public MovieSession add(MovieSession moveSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            Long movieId = (Long) session.save(moveSession);
+            transaction.commit();
+            moveSession.setId(movieId);
+            return moveSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert moveSession entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
