a9bfeb8827c175ff52ea1aa1754b93614f41e53c
Laszlo Petechuk <frostpv@gmail.com>
2020-10-07 21:48:01 +0300

    added AuthenticationServiceImpl and exceptions

---
+++ src/main/java/com/dev/cinema/service/AuthenticationServiceImpl.java
@@ -0,0 +1,44 @@
+package com.dev.cinema.service;
+
+import com.dev.cinema.exceptions.AuthenticationException;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HashUtil;
+import java.util.Optional;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDb = userService.findByEmail(email);
+        if (userFromDb.isEmpty()) {
+            throw new AuthenticationException("Incorrect email " + email);
+        }
+        User user = userFromDb.get();
+        if (isValid(user.getPassword(), password, user.getSalt())) {
+            return user;
+        }
+        throw new AuthenticationException("Incorrect password");
+    }
+
+    @Override
+    public User register(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDb = userService.findByEmail(email);
+        if (userFromDb.isPresent()) {
+            throw new AuthenticationException("This email " + email + " is already used");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        return userService.add(user);
+    }
+
+    private boolean isValid(String userPassword, String password, byte[] salt) {
+        return userPassword.equals(HashUtil.hashPassword(password, salt));
+    }
+}
+
