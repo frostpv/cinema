21c953255f56899c34fbcf20fabfcbb9b43350bc
Laszlo Petechuk <frostpv@gmail.com>
2020-10-08 08:40:52 +0300

    fixed alert

--- src/main/java/com/dev/cinema/dao/UserDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/UserDaoImpl.java
@@ -25,7 +25,7 @@ public class UserDaoImpl implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't add user " + user + " in to DB", e);
+            throw new DataProcessingException("Can't add user " + user + " in to the DB", e);
         } finally {
             if (session != null) {
                 session.close();
