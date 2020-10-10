9e53157a95e3a5d98fc75b484e13fa54a9c2f898
Laszlo Petechuk <frostpv@gmail.com>
2020-10-10 16:01:58 +0300

    edit AuthenticationServiceImpl

--- src/main/java/com/dev/cinema/service/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/service/AuthenticationServiceImpl.java
@@ -11,6 +11,8 @@ import java.util.Optional;
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
     private UserService userService;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
@@ -25,6 +27,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User register(String email, String password) {
         User user = new User(email, password);
+        shoppingCartService.registerNewShoppingCart(user);
         return userService.add(user);
     }
 
