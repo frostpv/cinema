bbc12fd5b0190f5e37c446f9e755ec722fefa885
Laszlo Petechuk <frostpv@gmail.com>
2020-10-10 15:47:53 +0300

    add ShoppingCartService

---
+++ src/main/java/com/dev/cinema/dao/TicketDaoImpl.java
@@ -0,0 +1,32 @@
+package com.dev.cinema.dao;
+
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.model.Ticket;
+import com.dev.cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class TicketDaoImpl implements TicketDao {
+    @Override
+    public Ticket add(Ticket ticket) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(ticket);
+            transaction.commit();
+            return ticket;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert ticket entity"
+                    + ticket + " in to data", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
