356c63039b616941c4c8b023a1c96e428f0ddfed
Laszlo Petechuk <frostpv@gmail.com>
2020-10-12 17:10:05 +0300

    Add OrderServiceImpl

--- src/main/java/com/dev/cinema/service/OrderServiceImpl.java
+++ src/main/java/com/dev/cinema/service/OrderServiceImpl.java
@@ -1,22 +1,31 @@
 package com.dev.cinema.service;
 
+import com.dev.cinema.dao.OrderDao;
+import com.dev.cinema.lib.Inject;
 import com.dev.cinema.lib.Service;
 import com.dev.cinema.model.Order;
 import com.dev.cinema.model.Ticket;
 import com.dev.cinema.model.User;
-
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(List<Ticket> tickets, User user) {
+        Order order = new Order(tickets, user, LocalDateTime.now());
+        orderDao.add(order);
+        shoppingCartService.clear(shoppingCartService.getByUser(user));
         return null;
     }
 
     @Override
     public List<Order> getOrderHistory(User user) {
-        return null;
+        return orderDao.getOrderHistory(user);
     }
 }
