0a3002fee802136f4d9259f982b3b852cbd8204e
Laszlo Petechuk <frostpv@gmail.com>
2020-10-12 16:45:55 +0300

    added OrderDao impl

--- src/main/java/com/dev/cinema/dao/OrderDao.java
+++ src/main/java/com/dev/cinema/dao/OrderDao.java
@@ -1,12 +1,11 @@
 package com.dev.cinema.dao;
 
 import com.dev.cinema.model.Order;
-import com.dev.cinema.model.Ticket;
 import com.dev.cinema.model.User;
 import java.util.List;
 
 public interface OrderDao {
-    Order completeOrder(List<Ticket> tickets, User user);
+    Order add(Order order);
 
     List<Order> getOrderHistory(User user);
 }
---
+++ src/main/java/com/dev/cinema/dao/OrderDaoImpl.java
@@ -0,0 +1,46 @@
+package com.dev.cinema.dao;
+
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.model.Order;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert order entity"
+                    + order + " into the database", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Order> getOrderHistory(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Order "
+                    + "JOIN FETCH Order.user "
+                    + "LEFT JOIN FETCH Order.tickets "
+                    + "WHERE Order.user = :user", Order.class)
+                    .setParameter("user", user)
+                    .getResultList();
+        }
+    }
+}
