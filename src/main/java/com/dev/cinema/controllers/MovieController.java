2592368c1cb831a420cf64763e7538c7a9eaa3e2
Laszlo Petechuk <frostpv@gmail.com>
2020-10-22 19:48:10 +0300

    add movie DTOs

--- src/main/java/com/dev/cinema/controllers/MovieController.java
+++ src/main/java/com/dev/cinema/controllers/MovieController.java
@@ -1,7 +1,21 @@
 package com.dev.cinema.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
+
+    @PostMapping("/")
+    public void addMovie() {
+
+    }
+
+    @GetMapping("/")
+    public void getMovie() {
+
+    }
 }
---
+++ src/main/java/com/dev/cinema/dto/movie/MovieResponseDto.java
@@ -0,0 +1,10 @@
+package com.dev.cinema.dto.movie;
+
+import lombok.Data;
+
+@Data
+public class MovieResponseDto {
+    private Long id;
+    private String title;
+    private String description;
+}
