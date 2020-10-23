e1cdde9627fa092fc58a6ef9571ed48cbdfadd54
Laszlo Petechuk <frostpv@gmail.com>
2020-10-23 11:23:15 +0300

    fixed MovieSessionController

--- src/main/java/com/dev/cinema/controllers/AuthenticationController.java
+++ src/main/java/com/dev/cinema/controllers/AuthenticationController.java
@@ -2,7 +2,6 @@ package com.dev.cinema.controllers;
 
 import com.dev.cinema.dto.user.UserRequestDto;
 import com.dev.cinema.mapper.UserMapper;
-import com.dev.cinema.model.User;
 import com.dev.cinema.service.AuthenticationService;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/dev/cinema/controllers/MovieSessionController.java
+++ src/main/java/com/dev/cinema/controllers/MovieSessionController.java
@@ -3,7 +3,6 @@ package com.dev.cinema.controllers;
 import com.dev.cinema.dto.session.MovieSessionRequestDto;
 import com.dev.cinema.dto.session.MovieSessionResponseDto;
 import com.dev.cinema.mapper.MovieSessionMapper;
-import com.dev.cinema.model.MovieSession;
 import com.dev.cinema.service.MovieSessionService;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/movie-sessions")
 public class MovieSessionController {
-    private static final String DATE_FORMAT = "d.MM.yyyy";
     private final MovieSessionService movieSessionService;
     private final MovieSessionMapper movieSessionMapper;
 
@@ -36,11 +34,9 @@ public class MovieSessionController {
     @GetMapping("/available")
     public List<MovieSessionResponseDto> getAllAvailable(@RequestParam Long movieId,
                                                          @RequestParam String date) {
-        List<MovieSession> movieSessions =
-                movieSessionService.findAvailableSessions(movieId,
-                        LocalDate.parse(date, DateTimeFormatter.ofPattern(DATE_FORMAT)));
-
-        return movieSessions.stream()
+        return movieSessionService.findAvailableSessions(movieId,
+                LocalDate.parse(date, DateTimeFormatter.ofPattern("d.MM.yyyy")))
+                .stream()
                 .map(movieSessionMapper::movieSessionToDto)
                 .collect(Collectors.toList());
     }
