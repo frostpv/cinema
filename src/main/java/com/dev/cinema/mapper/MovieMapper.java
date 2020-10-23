6890ad7ce9b3837f648f36021348a57463f023ec
Laszlo Petechuk <frostpv@gmail.com>
2020-10-23 11:24:18 +0300

    removed empty line

--- src/main/java/com/dev/cinema/mapper/MovieMapper.java
+++ src/main/java/com/dev/cinema/mapper/MovieMapper.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MovieMapper {
-
     public Movie dtoToMovie(MovieRequestDto movieRequestDto) {
         Movie movie = new Movie();
         movie.setTitle(movieRequestDto.getTitle());
