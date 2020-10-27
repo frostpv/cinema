b2fd35e324fe4349d21f482e50043a4105055e46
Laszlo Petechuk <frostpv@gmail.com>
2020-10-27 20:15:06 +0200

    add OrderController logic

--- src/main/java/com/dev/cinema/controllers/OrderController.java
+++ src/main/java/com/dev/cinema/controllers/OrderController.java
@@ -1,23 +1,47 @@
 package com.dev.cinema.controllers;
 
+import com.dev.cinema.dto.order.OrderResponseDto;
+import com.dev.cinema.mapper.OrderMapper;
 import com.dev.cinema.model.Order;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.OrderService;
+import com.dev.cinema.service.ShoppingCartService;
+import com.dev.cinema.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+    private final ShoppingCartService shoppingCartService;
+    private final OrderService orderService;
+    private final UserService userService;
+    private final OrderMapper orderMapper;
+
+    public OrderController(ShoppingCartService shoppingCartService,
+                           OrderService orderService,
+                           UserService userService,
+                           OrderMapper orderMapper) {
+        this.shoppingCartService = shoppingCartService;
+        this.orderService = orderService;
+        this.userService = userService;
+        this.orderMapper = orderMapper;
+    }
 
     @PostMapping("/complete")
-    public void completeOrder() {
-
+    public void completeOrder(@RequestParam Long userId) {
+        User user = userService.findById(userId);
+        orderService.completeOrder(shoppingCartService.getByUser(user).getTickets(), user);
     }
 
     @GetMapping
-    public List<Order> getUserOrder(@RequestParam Long userId){
-        return new ArrayList<>();
+    public List<OrderResponseDto> getUserOrder(@RequestParam Long userId){
+        return orderService.getOrderHistory(userService.findById(userId))
+                .stream()
+                .map(orderMapper::orderToDto)
+                .collect(Collectors.toList());
     }
-
 }
