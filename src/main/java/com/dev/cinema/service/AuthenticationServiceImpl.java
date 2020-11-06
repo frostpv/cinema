3197193b77707211ded579c9f4239d6d0baf96d8
Laszlo Petechuk <frostpv@gmail.com>
2020-11-06 16:01:15 +0200

    fixed AuthenticationServiceImpl

--- src/main/java/com/dev/cinema/service/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/service/AuthenticationServiceImpl.java
@@ -1,21 +1,27 @@
 package com.dev.cinema.service;
 
 import com.dev.cinema.model.User;
+import java.util.Set;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
+    private final RoleService roleService;
 
-    public AuthenticationServiceImpl(UserService userService, ShoppingCartService cartService) {
+    public AuthenticationServiceImpl(UserService userService,
+                                     ShoppingCartService cartService,
+                                     RoleService roleService) {
         this.userService = userService;
         this.shoppingCartService = cartService;
+        this.roleService = roleService;
     }
 
     @Override
     public User register(String email, String password) {
         User user = new User(email, password);
+        user.setRoles(Set.of(roleService.getRoleByName("USER")));
         User userFromDb = userService.add(user);
         shoppingCartService.registerNewShoppingCart(userFromDb);
         return userFromDb;
